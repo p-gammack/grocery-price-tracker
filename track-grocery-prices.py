877555f87ccca5e1998f70514d81f8c095203f43
Patrick Gammack <impatrickgammack@gmail.com>
2022-12-21 13:52:23 +0000

    moved html request and parse to functions

--- track-grocery-prices.py
+++ track-grocery-prices.py
@@ -15,16 +15,26 @@ sainsburys_unsalted_butter = {
     "url": 'https://www.sainsburys.co.uk/gol-ui/product/sainsburys-english-butter--unsalted-250g'
 }
 
-page = requests.get(waitrose_unsalted_butter["url"])
-soup = BeautifulSoup(page.content, "html.parser")
+def get_waitrose_unsalted_butter_price_data():
+    page = requests.get(waitrose_unsalted_butter["url"])
+    soup = BeautifulSoup(page.content, "html.parser")
 
-content = soup.find(id="content")
-prices = content.find("section", class_=re.compile("productPricing."))
-price_per_unit = prices.find("span", attrs={'data-test': 'product-pod-price'})
-price_per_kg = prices.find("span", class_=re.compile("pricePerUnit."))
+    content = soup.find(id="content")
+    prices = content.find("section", class_=re.compile("productPricing."))
+    return prices
 
-price_per_unit_str = price_per_unit.text[1:]
-price_per_kg_str = price_per_kg.text[2:][:-4:]
+def get_waitrose_unsalted_butter_price_per_unit():
+    prices = get_waitrose_unsalted_butter_price_data()
+    price_per_unit = prices.find("span", attrs={'data-test': 'product-pod-price'})
+    return price_per_unit.text[1:]
+
+def get_waitrose_unsalted_butter_price_per_kg():
+    prices = get_waitrose_unsalted_butter_price_data()
+    price_per_kg = prices.find("span", class_=re.compile("pricePerUnit."))
+    return price_per_kg.text[2:][:-4:]
+
+waitrose_unsalted_butter["price_per_unit"] = get_waitrose_unsalted_butter_price_per_unit()
+waitrose_unsalted_butter["price_per_kg"] = get_waitrose_unsalted_butter_price_per_kg()
 
 now = datetime.datetime.now()
 date_str = str(now.date())
@@ -39,11 +49,11 @@ database = mysql.connector.connect(
 dbcursor = database.cursor()
 
 sql = "INSERT INTO unsalted_butter (Date, Price_Per_Unit, Price_Per_KG) VALUES (%s, %s, %s)"
-sql_val = (date_str, price_per_unit_str, price_per_kg_str)
+sql_val = (date_str, waitrose_unsalted_butter["price_per_unit"], waitrose_unsalted_butter["price_per_kg"])
 dbcursor.execute(sql, sql_val)
 # database.commit()
 
 print(dbcursor.rowcount, "record inserted.")
 print('%s: Current price of Unsalted Butter at Waitrose is £%s £%s' %
-      (date_str, price_per_unit_str, price_per_kg_str))
+      (date_str, waitrose_unsalted_butter["price_per_unit"], waitrose_unsalted_butter["price_per_kg"]))
 time.sleep(5)
